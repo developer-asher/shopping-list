0aaf2d6d54370f7bb128c426f8cef9765c166e47
coder.asher <grit.developer.asher@gmail.com>
2021-03-22 17:04:25 +0900

    Add saving shopping list in local storage

--- js/main.js
+++ js/main.js
@@ -1,36 +1,71 @@
-function removeList(event) {
-    event.target.parentNode.remove();
+const form = document.querySelector('form');
+const input = document.querySelector('#input_list');
+const shoppingLists = document.querySelector('.lists');
+const btnPlus = document.querySelector('.btn_plus');
+
+let shopping = [];
+
+function deleteList(event) {
+  const li = event.target.parentNode;
+
+  li.remove();
+  const cleanShopping = shopping.filter(ls_list => ls_list.id != li.id);
+  
+  shopping = cleanShopping;
+  saveToDoList();
+}
+
+function saveToDoList() {
+  localStorage.setItem('userShoppingList', JSON.stringify(shopping));
+}
+
+function printShoppingList(value) {
+  const li = document.createElement('li');
+  const listId = `list${shopping.length + 1}`;
+  const trash = document.createElement('i');
+
+  trash.setAttribute('class', 'fas fa-trash-alt');
+  li.classList.add('list');
+  li.id = listId;
+  li.textContent = value;
+  li.appendChild(trash);
+  trash.addEventListener('click', deleteList);
+  shoppingLists.appendChild(li);
+
+  const listObj = {
+    text: value,
+    id: listId
+  };
+  shopping.push(listObj);
+
+  saveToDoList();
 }
 
 function handleShoppingList(event) {
-  const input = event.target.querySelector('#input_list');
-  const shoppingLists = document.querySelector('.lists');
-  const list = document.createElement('li');
-  const removeIcon = '<i class="fas fa-trash-alt"></i>';
-
   event.preventDefault();
-  list.classList.add('list');
-  list.innerHTML = `
-      ${input.value}
-      ${removeIcon}
-  `;
-  shoppingLists.appendChild(list);
+
+  const list = input.value;
+  printShoppingList(list);
   input.value = '';
-
-
-  const btnRemove = document.querySelectorAll('.list>i');
-  btnRemove.forEach(ele => ele.addEventListener('click', removeList));
 }
 
 function handleEventListener() {
-  const form = document.querySelector('form');
-  const btnPlus = document.querySelector('.btn_plus');
-
+  form.addEventListener('submit', handleShoppingList);
   btnPlus.addEventListener('click', handleShoppingList);
-  form.addEventListener('submit', (event) => handleShoppingList(event));
+}
+
+function loadShoppingList() {
+  const loadedShoppingList = localStorage.getItem('userShoppingList');
+
+  if(loadedShoppingList !== null) {
+    const parsedList = JSON.parse(loadedShoppingList);
+
+    parsedList.forEach(list => printShoppingList(list.text));
+  }
 }
 
 function init() {
+  loadShoppingList();
   handleEventListener();
 }
 
